2c4a1130aa8b3690514ec29844e882b88dee8d80
lenny <lennytmp@gmail.com>
2024-01-26 22:30:28 +0100

    Add the behaviour to drop food and for it to appear again

--- Assets/FoodItem.cs
+++ Assets/FoodItem.cs
@@ -2,25 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class FoodBehaviour : MonoBehaviour
 {
     public float moveSpeed = 0f;
+    public bool isDropped = false;
     public string keyCode = "";
+
+    public Vector3 initialPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPos = transform.position;
     }
 
     private void drop() {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 1f;
+        isDropped = true;
+        StartCoroutine(DelayedClone());
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isDropped) {
+            return;
+        }
         if (keyCode == "Q" && Input.GetKeyDown(KeyCode.Q))
         {
             drop();    
@@ -43,5 +51,13 @@ public class NewBehaviourScript : MonoBehaviour
     void OnCollisionEnter2D(Collision collision)
     {
         moveSpeed = 1f;
+    }
+
+    IEnumerator DelayedClone()
+    {
+        yield return new WaitForSeconds(5f);
+        GameObject clone = Instantiate(gameObject, initialPos, transform.rotation);
+        clone.GetComponent<FoodBehaviour>().isDropped = false;
+        clone.GetComponent<Rigidbody2D>().gravityScale = 0f;
     }
 }
